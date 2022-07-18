fb1b651829174a86b00f63b5855e6d2fe04db244
chrzanof <filipchrzanowski94@gmail.com>
2022-07-18 14:28:11 +0200

    test4

--- src/com/company/A.java
+++ src/com/company/A.java
@@ -12,5 +12,6 @@ public class A {
         System.out.println("another test1");
         System.out.println("another test2");
         System.out.println("another test3");
+        System.out.println("another test4");
     }
 }
