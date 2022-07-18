849c57f2a0091ba8daff80c4f4f210b41688119b
chrzanof <filipchrzanowski94@gmail.com>
2022-07-18 13:43:40 +0200

    newFunc() in A
    added new function

--- src/com/company/A.java
+++ src/com/company/A.java
@@ -4,4 +4,7 @@ public class A {
     public void test() {
         System.out.println("nie lubie github");
     }
+    public void newFunc() {
+        System.out.println("why branch protection is only for paid users???");
+    }
 }
